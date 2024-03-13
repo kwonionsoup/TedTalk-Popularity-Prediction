23364f87e1f125fa3d3b44af2447a1ca20772b0d
Alexandra Pfleegor <apfleegor6@gatech.edu>
2024-03-12 23:21:14 -0400

    processed data without weird data breaks
    Here is the processed data ignoring all weird instances of transcripts being on 2 lines. As a note, in data_transcript_fully_processed.csv, processed_transcript refers to the column where I removed stopwords and lemmatized. I could not get the version with no contractions to also upload in the same file.

--- text_processing.py
+++ text_processing.py
@@ -3,14 +3,62 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize
 import contractions
-
 import string
 
-df_org = pd.read_csv("data/talks_info_noedits.csv")
-df = df_org[['_id', 'duration', 'likes', 'speakers', 'subtitle_languages', 'summary', 'title', 'transcript', 'views', 'recorded_date']]
-df["transcript"] = df.transcript.astype("string")
-
+# global vars
 stopws = stopwords.words("english")
+
+def michael_preprocess(data_file):
+    """
+    Drops NaN values within the columns "likes", "duration", and "views" of the TED_info data and saves it to a new excel file.
+
+    Args:
+        data_file (str): Path to the Excel file
+    
+    Returns:
+        pandas.DataFrame:
+    """
+
+    df = pd.read_csv(data_file)
+    df = df[['_id', 'duration', 'likes', 'speakers', 'subtitle_languages', 'summary', 'title', 'transcript', 'views']]
+
+    # add published date
+    # df['published_date'] = pd.to_datetime(df['published_date'], errors='coerce').dt.date
+    
+    # df['likes'] used to be an object data type, not int
+    df['likes'] = df['likes'].astype('str')
+    df['likes'] = df['likes'].apply(convert_likes)
+   
+    # Drop rows with NaN values
+    df_dropna = df.dropna(subset=['likes', 'duration', 'views'])
+
+    return df_dropna
+
+def convert_likes(likes_str):
+    # Remove non-numeric characters and convert to numeric
+    if likes_str[-1] == 'K':
+        return int(float(likes_str.replace('K', '')) * 1000)
+    elif likes_str[-1] == 'M':
+        return int(float(likes_str.replace('M', '')) * 1000000)
+    else:
+        return int(likes_str)
+
+def main():
+
+    data_path = 'data\dropna_processed_deleted_weird.csv'
+
+    df = michael_preprocess(data_path)
+    # df = df_org[['_id', 'duration', 'likes', 'speakers', 'subtitle_languages', 'summary', 'title', 'transcript', 'views', 'recorded_date']]
+    df["transcript"] = df.transcript.astype("string")
+
+    final_transcript = df.copy()
+    final_transcript.fillna("nan",inplace=True)
+    final_transcript["processed_transcript"] = final_transcript["transcript"].apply(process_text)
+    # final_transcript["transcript_no_contractions"] = final_transcript["transcript"].apply(process_text2)
+
+    final_transcript.to_csv("data/data_transcript_fully_processed.csv")
+
+    
 def process_text(conversation):
     
     # expand contractions
@@ -38,5 +86,14 @@ def process_text(conversation):
 
     return final_text
 
-transcript1 = df[df["_id"]=="21"]["transcript"]
-print(process_text(transcript1[0]))
+def process_text2(conversation):
+    
+    if conversation!='nan':
+        contractions_stuff = [contractions.fix(word) for word in conversation.split()]
+        conversation = ' '.join(contractions_stuff)
+        
+    return conversation
+
+
+if __name__ == "__main__":
+    main()
